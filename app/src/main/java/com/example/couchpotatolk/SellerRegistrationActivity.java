b3b26713eaf0449aa7fd2f35e14ba9141c201799
Sheikha <sheikhahanna@gmail.com>
2020-10-05 23:09:10 +0530

    changes

---
+++ app/src/main/java/com/example/couchpotatolk/SellerRegistrationActivity.java
@@ -0,0 +1,146 @@
+package com.example.couchpotatolk;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class SellerRegistrationActivity extends AppCompatActivity {
+
+
+    //private Button sellerLoginBegin;
+    private EditText nameInput, phoneInput, emailInput, passwordInput, websiteInput, descInput;
+    private Button registerButton;
+
+    AwesomeValidation awesomeValidation;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog loadingbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_registration_activity);
+
+        //Initialize Validation Styles
+                awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        //validations for variables
+        awesomeValidation.addValidation(this,R.id.seller_name,
+                RegexTemplate.NOT_EMPTY,R.string.invalid_name);
+
+        awesomeValidation.addValidation(this,R.id.seller_phone,
+                "[5-1]{1}[0-9]{9}$",R.string.invalid_mobile);
+
+        awesomeValidation.addValidation(this,R.id.seller_email,
+                Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+
+        awesomeValidation.addValidation(this,R.id.seller_password,
+                Patterns.EMAIL_ADDRESS,R.string.invalid_password);
+
+        awesomeValidation.addValidation(this,R.id.seller_website,
+                Patterns.WEB_URL,R.string.invalid_website);
+
+        mAuth = FirebaseAuth.getInstance();
+        loadingbar = new ProgressDialog(this);
+
+        //sellerLoginBegin = findViewById(R.id.seller_alreadyhave_btn);
+        registerButton = findViewById(R.id.seller_register_btn);
+        nameInput = findViewById(R.id.seller_name);
+        phoneInput = findViewById(R.id.seller_phone);
+        emailInput = findViewById(R.id.seller_email);
+        passwordInput = findViewById(R.id.seller_password);
+        websiteInput = findViewById(R.id.seller_website);
+        descInput = findViewById(R.id.seller_desc);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check Validation
+                if(awesomeValidation.validate()){
+                    //On Success
+                    Toast.makeText(getApplicationContext(),"Validation Successful",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(),
+                            "Validation Failed",Toast.LENGTH_SHORT).show();
+                }
+
+                registerSeller();
+            }
+        });
+    }
+
+    private void registerSeller() {
+        String name = nameInput.getText().toString();
+        final String phone = phoneInput.getText().toString();
+        final String email = emailInput.getText().toString();
+        final String password = passwordInput.getText().toString();
+        final String website = websiteInput.getText().toString();
+        final String desc = descInput.getText().toString();
+
+        if (!name.equals("") && !phone.equals("") && !email.equals("") && !password.equals("") && !website.equals("") && !desc.equals(""))
+        {
+            loadingbar.setTitle("Creating Vendor Account");
+            loadingbar.setMessage("Please wait while we are checking the credentials");
+            loadingbar.setCanceledOnTouchOutside(false);
+            loadingbar.show();
+
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                final DatabaseReference rootRef;
+                                rootRef = FirebaseDatabase.getInstance().getReference();
+
+                                String sid = mAuth.getCurrentUser().getUid();
+
+                                HashMap<String,Object> sellerMap = new HashMap<>();
+                                sellerMap.put("sid",sid);
+                                sellerMap.put("phone",phone);
+                                sellerMap.put("email",email);
+                                sellerMap.put("password",password);
+                                sellerMap.put("website",website);
+                                sellerMap.put("desc",desc);
+
+                                rootRef.child("Sellers").child(sid).updateChildren(sellerMap)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                loadingbar.dismiss();
+                                                Toast.makeText(SellerRegistrationActivity.this, "Registered Successfully.",Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+
+
+                            }
+                        }
+                    });
+        }
+        else
+        {
+            Toast.makeText(this,"Please fill in the form to register",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
