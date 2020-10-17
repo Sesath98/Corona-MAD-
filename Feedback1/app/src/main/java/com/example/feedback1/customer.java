aff8316e69bfca1a2122b151cc3cf83a09b779c6
fadhir266 <70846721+fadhir266@users.noreply.github.com>
2020-10-17 14:10:52 +0530

    Add files via upload

---
+++ Feedback1/app/src/main/java/com/example/feedback1/customer.java
@@ -0,0 +1,37 @@
+package com.example.feedback1;
+
+public class customer {
+
+    private String name;
+    private String email;
+    private String message;
+
+    public customer() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
